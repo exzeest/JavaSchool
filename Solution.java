8f1fec875b60a81dcbeca98b71d75925d5382ae1
exzeest <exzeest@gmail.com>
2017-08-21 01:20:59 +0300

    2004. Високосный год

--- Solution.java
+++ Solution.java
@@ -3,16 +3,8 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int count = scanner.nextInt();
-        int sum = 0;
-        for (int i = 0; i < count; i++) {
-            if (i % 2 == 0)
-                sum += scanner.nextInt();
-            else
-                sum -= scanner.nextInt();
-        }
-
-        System.out.println(sum);
-
+        int year = scanner.nextInt();
+        int result = ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))? 1 : 0;
+        System.out.println(result);
     }
 }
